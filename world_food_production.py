a494bd85edfd2ea073ef4a83436df4cbd2d489f6
Neve <nevecoward14@gmail.com>
2020-03-28 23:32:14 +0000

    added titles and made graphs
    have messaged laura bc graphs have way too much data atm to actually be useful

--- world_food_production.py
+++ world_food_production.py
@@ -1,31 +1,46 @@
 import csv
 from operator import itemgetter
+import matplotlib.pyplot as plt
+import numpy as np
 
 with open('world_food_production_2018.csv', 'r') as csv_file:
     food_data = csv.DictReader(csv_file)
-
     headers = food_data.fieldnames
-    print(headers)
 
     food_data =list(food_data)
+    print()
+    class color:
+        PURPLE = '\033[95m'
+        CYAN = '\033[96m'
+        DARKCYAN = '\033[36m'
+        BLUE = '\033[94m'
+        GREEN = '\033[92m'
+        YELLOW = '\033[93m'
+        RED = '\033[91m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+        END = '\033[0m'
 
+
+    print(color.BOLD + color.UNDERLINE + 'BIGGEST PRODUCER OF EACH FOOD TYPE BY COUNTRY (2018):'+ color.END)
     for food in headers[2:]:
         country_data = max(food_data, key=lambda country: int(country[food]))
         tonnes = int(country_data[food])
         food_formatted = food.replace('_', ' ')
-        print('In 2018, the country which produced the most {} was {}, and they produced {} tonnes.'.format(food_formatted, country_data['Country'], tonnes))
+        print('{} produced the most {}: {} tonnes.'.format(country_data['Country'], food_formatted, tonnes) +'\n')
 
-        areas = {
-        'asia': {},
-        'europe': {},
-        'africa': {},
-        'middle east': {},
-        'north america': {},
-        'south america': {},
-        'south atlantic': {},
-        'oceania': {}
+    areas = {
+    'asia': {},
+    'europe': {},
+    'africa': {},
+    'middle east': {},
+    'north america': {},
+    'south america': {},
+    'south atlantic': {},
+    'oceania': {}
     }
 
+    print(color.BOLD + color.UNDERLINE + 'BIGGEST PRODUCER OF EACH FOOD TYPE BY REGION (2018):' + color.END)
     for country in food_data:
         area = country['Area'].lower()
         for food in headers[2:]:
@@ -38,12 +53,30 @@ with open('world_food_production_2018.csv', 'r') as csv_file:
         area = max(areas.keys(), key=lambda area: areas[area][food])
         tonnes = int(areas[area][food])
         food_formatted = food.replace('_', ' ')
-        print('In 2018, the area which produced the most {} was {}, and they produced {} tonnes.'.format(food_formatted, area, tonnes))
+        print('{} produced the most {}: {} tonnes.'.format(area, food_formatted, tonnes) + '\n')
 
-    desired_country = input('What area do you want to know about: Asia, Africa, Europe, North America, South America, Middle east, Oceania or South Atlantic').lower().strip()
+    desired_country = input(color.BOLD +'WHICH REGION WOULD YOU LIKE TO KNOW MORE ABOUT? Asia, Africa, Europe, North America, South America, Middle east, Oceania or South Atlantic'+ color.END).lower().strip()
+    print('Here is the weight in tonnes per food group produced by that region in 2018:')
     print(areas[desired_country])
+    print('\n')
 
-    ################################################################################################neve above, fenella below
+
+    citrus_country = np.array(())
+    for country in food_data:
+        citrus_country = np.append(citrus_country, int(country['Citrus_fruit']))
+
+    country_name = np.array (())
+    for country in food_data:
+        country_name = np.append(country_name, (country['Country']))
+
+    plt.figure()
+    plt.scatter(country_name, citrus_country)
+    plt.ylabel('Tonnes')
+    plt.xlabel('Country')
+    plt.title('Tonnes of citrus produced by country')
+    plt.show()
+
+    ###############################################################################################################################
 
     country_names = []
     total_prod = []
